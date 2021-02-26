b8792b96002b5cc378276fb553a0846cac7dca48
jys <runnway@naver.com>
2021-02-26 15:59:21 +0900

    go의 함수와 에러핸들링

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,5 +1,7 @@
 package accounts
 
+import "errors"
+
 // Account struct
 type Account struct {
 	owner   string
@@ -10,4 +12,25 @@ type Account struct {
 func NewAccount(owner string) *Account {
 	account := Account{owner: owner, balance: 0}
 	return &account
+}
+
+// receiver a 를 이요하여 변수를 수정할 수 있다. struct에 속한 함수
+// 선언된 변수를 사용하는것이 아니라 복사해서 사용하기 떄문에 직접적 변화에 작동하지 않는다
+// 그래서 포인터를 통해서 해당 주소의 값을 직접 변경해야함
+func (a *Account) Deposit(amount int) {
+	a.balance += amount
+}
+
+// Balance of user account
+func (a Account) Balance() int {
+	return a.balance
+}
+
+// Withdraw balance with Error handling
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errors.New("balnce is not enough")
+	}
+	a.balance -= amount
+	return nil
 }
--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/nakusha/learngo/accounts"
 )
 
 func main() {
 	account := accounts.NewAccount("Yeonsu")
-	fmt.Println(*account)
+	account.Deposit(100)
+	fmt.Println(account.Balance())
+	err := account.Withdraw(10)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(account.Balance())
 }
